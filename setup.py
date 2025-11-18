6a3269be708b99c84d1d844b9dee286013b77590
sajeeahrasmi <sajeeahrasmi@gmail.com>
2025-11-18 11:38:07 +0530

    added req

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medicalChatbot",
+    version="0.1.0",
+    author="Sajeea Rasmi",
+    author_email="sajeeahrasmi@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
